2e84c5934a0513533b3cc21567bdb7c7ac91a3be
PresentKim <debe3721@gmail.com>
2021-12-01 20:23:00 +0900

    MapImageUtils : Add fromPixels() - Resize via nearest-neighbor algorithm

--- src/ref/api/mapmanager/utils/MapImageUtils.php
+++ src/ref/api/mapmanager/utils/MapImageUtils.php
@@ -26,10 +26,13 @@ declare(strict_types=1);
 
 namespace ref\api\mapmanager\utils;
 
+use InvalidArgumentException;
 use pocketmine\color\Color;
 use pocketmine\network\mcpe\protocol\types\MapImage;
 
 use function array_fill;
+use function count;
+use function intdiv;
 
 final class MapImageUtils{
     private function __construct(){ }
@@ -39,7 +42,41 @@ final class MapImageUtils{
         return new MapImage(array_fill(0, $targetHeight, array_fill(0, $targetWidth, $color)));
     }
 
+    /** Auto resize with nearest-neighbor algorithm */
+    public static function fromPixels(array $input, int $targetWidth, int $targetHeight) : MapImage{
+        $sourceWidth = null;
+        foreach($input as $row){
+            if($sourceWidth === null){
+                $sourceWidth = count($row);
+            }elseif(count($row) !== $sourceWidth){
+                throw new InvalidArgumentException("All rows must have the same number of pixels");
+            }
+        }
+        $sourceHeight = count($input);
+        if($sourceWidth > $targetWidth || $sourceHeight > $targetHeight){
+            throw new InvalidArgumentException("Only map image enlargement is supported. Shrinkage is not supported.");
+        }
+
+        $output = [];
+        $x_ratio = intdiv($sourceWidth << 16, $targetWidth);
+        $y_ratio = intdiv($sourceHeight << 16, $targetHeight);
+        for($y = 0; $y < $targetHeight; ++$y){
+            $sourceY = ($y * $y_ratio) >> 16;
+            $output[$y] = [];
+            for($x = 0; $x < $targetWidth; ++$x){
+                $sourceX = ($x * $x_ratio) >> 16;
+                $output[$y][$x] = $input[$sourceY][$sourceX];
+            }
+        }
+        return new MapImage($output);
+    }
+
     public static function validateSize(MapImage $image) : bool{
         return $image->getWidth() > 0 && $image->getHeight() > 0 && $image->getWidth() <= 128 && $image->getHeight() <= 128;
+    }
+
+    /** Scale up with nearest-neighbor algorithm */
+    public static function resizeMapImage(MapImage $mapImage, int $targetWidth, int $targetHeight) : MapImage{
+        return self::fromPixels($mapImage->getPixels(), $targetWidth, $targetHeight);
     }
 }
